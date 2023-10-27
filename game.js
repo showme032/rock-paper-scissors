843fb688890bb31886a971c83677da8850fbb202
showme032 <sxomy993@gmail.com>
2023-10-27 14:13:44 +0200

    implement the full game of 5 rounds
    added a function which plays 5 rounds, prompting the user for a choice each time

--- game.js
+++ game.js
@@ -1,11 +1,21 @@
-// give a list of possible choices and return a random one
+// Prompt the user to choose his hand
+function getPlayerChoice() {
+    let hand = prompt("Which do you choose, Rock, Paper or Scissors?").toLowerCase()
+    return hand
+}
+
+
+// Choose a random hand for the computer
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'] 
     return choices[Math.floor(Math.random() * choices.length)]
-}
+}    
 
-//play a single round of game
+
+// Play a single round of game, comparein players hand with computers and declare a winner/tie
 function playRound(playerSelection, computerSelection) {
+    
+    // If player chose rock
     if (playerSelection === "rock") {
         if (computerSelection === "paper") {
             return "You lose, Paper beats Rock!";
@@ -14,21 +24,33 @@ function playRound(playerSelection, computerSelection) {
         } else {
             return "It's a tie!";
         }
+
+    // If player chose paper
     } else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            return "You lose, Scissors beat Paper!"
+            return "You lose, Scissors beat Paper!";
         } else if(computerSelection === "rock") {
-            return "You win, Paper beats Rock!"
+            return "You win, Paper beats Rock!";
         } else {
-            return "It's a tie!"
+            return "It's a tie!";
         }
-    } else {
+    
+    // If player chose scissors
+    } else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            return "You lose, Rock beats Scissors!"
+            return "You lose, Rock beats Scissors!";
         } else if(computerSelection === "paper") {
-            return "You win, Scissors beat Paper!"
+            return "You win, Scissors beat Paper!";
         } else {
-            return "It's a tie!"
+            return "It's a tie!";
         }
+    }
+}
+
+
+// Play a game of 5 rounds
+function game() {
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(getPlayerChoice(), getComputerChoice()))
     }
 }
