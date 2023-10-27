699826161aa422c7725a46df476e280e05787ee0
showme032 <sxomy993@gmail.com>
2023-10-27 13:34:30 +0200

    implement a round of rock-paper-scissors
    given two inputs, a player and a computer hand, the function declares a winner

--- game.js
+++ game.js
@@ -1,5 +1,34 @@
 // give a list of possible choices and return a random one
 function getComputerChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'] 
+    const choices = ['rock', 'paper', 'scissors'] 
     return choices[Math.floor(Math.random() * choices.length)]
+}
+
+//play a single round of game
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose, Paper beats Rock!";
+        } else if(computerSelection === "scissors") {
+            return "You win, Rock beats Scissors!";
+        } else {
+            return "It's a tie!";
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            return "You lose, Scissors beat Paper!"
+        } else if(computerSelection === "rock") {
+            return "You win, Paper beats Rock!"
+        } else {
+            return "It's a tie!"
+        }
+    } else {
+        if (computerSelection === "rock") {
+            return "You lose, Rock beats Scissors!"
+        } else if(computerSelection === "paper") {
+            return "You win, Scissors beat Paper!"
+        } else {
+            return "It's a tie!"
+        }
+    }
 }
